44904d013aae7b2e5b51cb15f4a1313a3c8080a9
VS-Vighnesh <vighneshsontakke3@gmail.com>
2025-06-11 08:39:10 +0530

    updated Q2

--- assignment_submission/Python_Day_1/Q2.py
+++ assignment_submission/Python_Day_1/Q2.py
@@ -1,9 +1,10 @@
-vowel = ["a","e","i","o","u"]
+def check_vowel(char):
+    match char:
+        case 'a' | 'e' | 'i' | 'o' | 'u':
+            return "Yes, it is a vowel"
+        case _:
+            return "No, it's not a vowel"
 
-x = input("Enter the character to check whether its a vowel or not : ").lower()
-if x in vowel:
-    out = "Yes, it is a vowel"
-    print(f"The character {x} is {out}")
-else:
-    out = "No, its not a vowel"
-    print(f"The character {x} is {out}")
+x = input("Enter the character to check whether it's a vowel or not: ").lower()
+out = check_vowel(x)
+print(f"The character {x} {out}")
